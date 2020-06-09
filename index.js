46026e1c207b111e5b2c8ed1189a95b20ee33cf5
Vast <svoutjekk@gmail.com>
2020-06-10 02:22:44 +0300

    upd

--- index.js
+++ index.js
@@ -1,47 +1,37 @@
-require('dotenv').config()
-const express = require('express')
-const telegram = require('node-telegram-bot-api')
-const logger = require('morgan')
+require('dotenv').config();
+require('colors');
 
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
-const adapter = new FileSync('db.json')
-const db = low(adapter)
-
+const express = require('express');
+const telegram = require('node-telegram-bot-api');
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const adapter = new FileSync(process.env.DATABASE_FILE || 'db.json');
+const db = low(adapter);
 const token = process.env.TELEGRAM_TOKEN;
 const bot = new telegram(token, {polling: true});
 
-db.defaults({chats: []}).write()
+db.defaults({chats: []}).write();
 
 bot.on('message', function (message) {
     const exist = db.get('chats').includes(message.chat.id).value();
     if (!exist) {
-        console.log('[telegram]', 'registered chat', message.chat.id)
+        console.log('[telegram]'.cyan, 'registered chat', message.chat.id)
         db.get('chats').push(message.chat.id).write();
     }
 })
 
 const app = express();
-app.use(logger('dev'));
 app.use(express.json());
 
 app.post("/alert", function (req, res) {
     const chats = db.get('chats').value()
-    const labels = req.body.commonLabels || {};
+    const labels = req.body['commonLabels'] || {};
     const message = Object.keys(labels).map(e => `${e}: ${labels[e]}`).join('\n')
-
-    if (!message) {
-        console.warn("[alert]", "got empty alert message")
-    } else {
-        console.log(message);
-    }
-
-    chats.forEach(e => {
-        console.log("[telegram]", "notifying chat", e);
-        bot.sendMessage(e, message);
-    });
-
+    console.log(`[alert]`.magenta, labels['severity'], labels['alert_type'], labels['alert_name']);
+    console.log("[telegram]".bold.cyan, "sending to", chats);
+    chats.forEach(e => bot.sendMessage(e, message).catch(ex => console.warn(e, ex)));
     res.status(200).json({});
 })
 
+app.use("*", (req, res) => res.status(404).json({message: "ENOTFOUND"}));
 app.listen(8080);
